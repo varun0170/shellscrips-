5cb4968468e8dc87dfd31a8d6cdb09186a042016
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2024-07-31 20:12:40 +0100

    Update readyforinstallation.sh

--- readyforinstallation.sh
+++ readyforinstallation.sh
@@ -21,7 +21,7 @@ sudo apt install ansible -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update -y
-sudo apt install jenkins
+sudo apt install jenkins -y
 sudo systemctl start jenkins
 
 #Install Docker
