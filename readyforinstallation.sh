8b89d4422d3ff77a29c62b6b82ee27b469cd432c
Varun Nalluri <108406501+varun0170@users.noreply.github.com>
2024-07-31 20:21:40 +0100

    Update readyforinstallation.sh

--- readyforinstallation.sh
+++ readyforinstallation.sh
@@ -29,7 +29,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 # Add the current user to the Docker group (to run Docker without sudo)
 sudo usermod -aG docker $USER
